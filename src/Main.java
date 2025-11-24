5e4e533f14d02af4328632d868184abc9ddf77d3
Harsh Patil <itsharshhh6@gmail.com>
2025-11-24 19:24:55 +0530

    N to 1

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,12 @@ public class Main {
         System.out.println(n);
     }
 
-//    private static void printNTo1(int n){
-//        if (n == 0) return;
-//        System.out.println(n);
-//        print1ToN(n - 1);
-//    }
-//
+    private static void printNTo1(int n){
+        if (n == 0) return;
+        System.out.println(n);
+        print1ToN(n - 1);
+    }
+
 //    private static int sumOfN(int n){
 //        if (n == 0) return 0;
 //        return n + sumOfN(n - 1);
@@ -32,10 +32,10 @@ public class Main {
         print1ToN(10);
         System.out.println();
 
-//        System.out.println("N to 1");
-//        printNTo1(10);
-//        System.out.println();
-//
+        System.out.println("N to 1");
+        printNTo1(10);
+        System.out.println();
+
 //        System.out.println("Sum of N numbers");
 //        System.out.println(sumOfN(10));
 //        System.out.println();
