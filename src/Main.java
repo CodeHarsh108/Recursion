f2680ff5bccf48a726feecad4d38e1d20de504ed
Harsh Patil <itsharshhh6@gmail.com>
2025-11-24 19:48:45 +0530

    Cleaned the code

--- src/Main.java
+++ src/Main.java
@@ -54,49 +54,64 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Message n times : ");
+
+        System.out.println("====================================");
+        System.out.println("         RECURSION TESTER");
+        System.out.println("====================================\n");
+
+        System.out.println("[1] Print message N times");
+        System.out.println("------------------------------------");
         printNTimes("Hello", 5);
         System.out.println();
 
-        System.out.println("1 to N");
+        System.out.println("[2] Print numbers 1 to N");
+        System.out.println("------------------------------------");
         print1ToN(10);
         System.out.println();
 
-        System.out.println("N to 1");
+        System.out.println("[3] Print numbers N to 1");
+        System.out.println("------------------------------------");
         printNTo1(10);
         System.out.println();
 
-        System.out.println("Sum of N numbers");
+        System.out.println("[4] Sum of first N numbers");
+        System.out.println("------------------------------------");
         System.out.println(sumOfN(10));
         System.out.println();
 
-        System.out.println("Factorial");
+        System.out.println("[5] Factorial");
+        System.out.println("------------------------------------");
         System.out.println(facorial(5));
         System.out.println();
 
-        System.out.println("Reverse an array : ");
+        System.out.println("[6] Reverse an array");
+        System.out.println("------------------------------------");
         int[] arr = {1, 2, 3, 4, 5};
         reverseArray(arr, 0, arr.length - 1);
         for (int x : arr) {
             System.out.print(x + " ");
         }
-        System.out.println();
+        System.out.println("\n");
 
-        System.out.println("Palindromic or not : ");
+        System.out.println("[7] Check palindrome");
+        System.out.println("------------------------------------");
         String str = "madam";
-
-        if (palindromic(str, 0, str.length() - 1))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not a palindrome");
-
+        System.out.println(palindromic(str, 0, str.length() - 1)
+                ? "Palindrome"
+                : "Not a palindrome");
         System.out.println();
 
-        System.out.println("Fibonacci number");
+        System.out.println("[8] Fibonacci number");
+        System.out.println("------------------------------------");
         System.out.println(fibonacci(6));
         System.out.println();
+
+        System.out.println("====================================");
+        System.out.println("              DONE");
+        System.out.println("====================================");
     }
 
 
 
-    }
+
+}
